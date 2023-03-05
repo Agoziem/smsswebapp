f930ce9f92ebc60ffc93afb7560ae763be0a0095
Agoziem <chiagoziendukwe@gmail.com>
2023-03-04 20:56:22 -0500

    changes

--- smsssite/settings.py
+++ smsssite/settings.py
@@ -167,6 +167,7 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS={'CacheControl':'max-age=86400'}
 AWS_S3_REGION_NAME = 'us-east-1'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE='smsssite.storages.MediaStore'
 
 AWS_LOCATION = 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "assets"),]
