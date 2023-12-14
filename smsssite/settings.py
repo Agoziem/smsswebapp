35ad2ebc9eeb1e084991cd64ebc6f727d8da9e6d
Agoziem <chiagoziendukwe90@gmail.com>
2023-12-14 10:47:54 -0800

    changes

--- smsssite/settings.py
+++ smsssite/settings.py
@@ -44,7 +44,8 @@ DJANGO_ADMIN_LOGS_DELETABLE = True
 DJANGO_ADMIN_LOGS_ENABLED = True
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
