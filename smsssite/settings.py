8241729123aed41cf1b1e9181efdbd8c2e5d20a7
Agoziem <chiagoziendukwe90@gmail.com>
2023-12-14 11:04:30 -0800

    Changes

--- smsssite/settings.py
+++ smsssite/settings.py
@@ -160,6 +160,7 @@ LOGIN_URL = 'Accounts:login'
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS= [os.path.join(BASE_DIR, "assets"),]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
